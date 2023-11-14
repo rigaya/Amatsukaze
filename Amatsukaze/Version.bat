0a246549ee03854f4ec56d5ab1e430ae7dbc4794
rigaya <rigaya34589@live.jp>
2023-11-14 16:02:49 +0900

    バージョン情報を焼き込む対象の実行ファイルを追加。

--- Amatsukaze/Version.bat
+++ Amatsukaze/Version.bat
@@ -1,3 +1,6 @@
 @echo off
-for /f "delims=" %%A in ('git describe --tags') do set VER=%%A
+for /f "usebackq delims=" %%A in (`git describe --tags`) do set VER=%%A
 echo #define AMATSUKAZE_VERSION "%VER%">Version.h
+
+for /f "usebackq delims= tokens=*" %%A in (`git describe "--abbrev=0" --tags`) do set VER=%%A
+echo #define AMATSUKAZE_PRODUCTVERSION %VER:.=,% >>Version.h
--- AmatsukazeGUI/Properties/AssemblyInfo.cs
+++ AmatsukazeGUI/Properties/AssemblyInfo.cs
@@ -1,55 +1,38 @@
-﻿using System.Reflection;
-using System.Resources;
+﻿
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("AmatsukazeGUI")]
+[assembly: AssemblyTitle("AmatsukazeServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AmatsukazeGUI")]
+[assembly: AssemblyProduct("AmatsukazeServer")]
 [assembly: AssemblyCopyright("Copyright (c)  2017-2019 Nekopanda")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから
-// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
 // その型の ComVisible 属性を true に設定してください。
 [assembly: ComVisible(false)]
 
-//ローカライズ可能なアプリケーションのビルドを開始するには、
-//.csproj ファイルの <UICulture>CultureYouAreCodingWith</UICulture> を
-//<PropertyGroup> 内部で設定します。たとえば、
-//ソース ファイルで英語を使用している場合、<UICulture> を en-US に設定します。次に、
-//下の NeutralResourceLanguage 属性のコメントを解除します。下の行の "en-US" を
-//プロジェクト ファイルの UICulture 設定と一致するよう更新します。
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("5bac25f7-ead0-4b4c-84a7-6edc9c4c8cd6")]
 
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //テーマ固有のリソース ディクショナリが置かれている場所
-    //(リソースがページ、
-    //またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
-    ResourceDictionaryLocation.SourceAssembly //汎用リソース ディクショナリが置かれている場所
-    //(リソースがページ、
-    //アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
-)]
-
-
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
 //      Revision
 //
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
-// 既定値にすることができます:
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 以下のように '*' を使用します:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.9.5.2")]
+[assembly: AssemblyInformationalVersion("0.9.5.2-4-ge82e52c")]
