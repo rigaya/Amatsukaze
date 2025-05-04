503aefcb3354b1033c3761965cefbac4b79f8442
rigaya <rigaya34589@live.jp>
2025-05-04 11:05:12 +0900

    Bitmap等の画像転送に関し、SixLabors.ImageSharpを使った転送に変更。

--- AmatsukazeServer/Lib/DefaultBitmapFactory.cs
+++ AmatsukazeServer/Lib/DefaultBitmapFactory.cs
@@ -1,21 +1,43 @@
 using System;
 using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
 
 namespace Amatsukaze.Lib
 {
     /// <summary>
-    /// 基本的なビットマップファクトリの実装
-    /// 実際の機能はプラットフォーム固有の実装が担当
+    /// ImageSharpを使用したビットマップファクトリの実装
+    /// Linuxでも使用可能な画像処理を提供します
     /// </summary>
     public class DefaultBitmapFactory : IBitmapFactory
     {
+        /// <summary>
+        /// 内部で使用するImage型のラッパークラス
+        /// 画像データと関連メタデータを保持します
+        /// </summary>
+        public class ImageWrapper
+        {
+            public Image<Rgb24> Image { get; }
+            public int Width => Image.Width;
+            public int Height => Image.Height;
+
+            public ImageWrapper(Image<Rgb24> image)
+            {
+                Image = image;
+            }
+        }
+
         /// <summary>
         /// バイト配列からビットマップを作成します
         /// </summary>
         public object CreateBitmapFromByteArray(byte[] buffer)
         {
-            // この実装は単にバイト配列を保持するだけ
-            return buffer;
+            using (var memoryStream = new MemoryStream(buffer))
+            {
+                return new ImageWrapper(Image.Load<Rgb24>(memoryStream));
+            }
         }
 
         /// <summary>
@@ -23,11 +45,7 @@ namespace Amatsukaze.Lib
         /// </summary>
         public object CreateBitmapFromStream(Stream stream)
         {
-            using (var memoryStream = new MemoryStream())
-            {
-                stream.CopyTo(memoryStream);
-                return memoryStream.ToArray();
-            }
+            return new ImageWrapper(Image.Load<Rgb24>(stream));
         }
 
         /// <summary>
@@ -35,7 +53,7 @@ namespace Amatsukaze.Lib
         /// </summary>
         public object CreateBitmapFromFile(string filePath)
         {
-            return File.ReadAllBytes(filePath);
+            return new ImageWrapper(Image.Load<Rgb24>(filePath));
         }
 
         /// <summary>
@@ -43,8 +61,29 @@ namespace Amatsukaze.Lib
         /// </summary>
         public object CreateBitmapFromRgb(byte[] buffer, int width, int height, int stride)
         {
-            // 単純にバッファを返す
-            return buffer;
+            var image = new Image<Rgb24>(width, height);
+            
+            // RGBバッファから画像を作成
+            image.ProcessPixelRows(accessor => 
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    int rowOffset = y * stride;
+                    var pixelRowSpan = accessor.GetRowSpan(y);
+                    
+                    for (int x = 0; x < width; x++)
+                    {
+                        int pixelOffset = rowOffset + x * 3;
+                        pixelRowSpan[x] = new Rgb24(
+                            buffer[pixelOffset],     // R
+                            buffer[pixelOffset + 1], // G
+                            buffer[pixelOffset + 2]  // B
+                        );
+                    }
+                }
+            });
+            
+            return new ImageWrapper(image);
         }
 
         /// <summary>
@@ -52,11 +91,8 @@ namespace Amatsukaze.Lib
         /// </summary>
         public void SaveBitmapAsJpeg(object bitmap, string filePath)
         {
-            var buffer = bitmap as byte[];
-            if (buffer == null)
-                throw new ArgumentException("無効なビットマップオブジェクトです", nameof(bitmap));
-
-            File.WriteAllBytes(filePath, buffer);
+            var wrapper = ValidateAndGetImageWrapper(bitmap);
+            wrapper.Image.Save(filePath, new JpegEncoder { Quality = 90 });
         }
 
         /// <summary>
@@ -64,11 +100,8 @@ namespace Amatsukaze.Lib
         /// </summary>
         public void SaveBitmapAsPng(object bitmap, string filePath)
         {
-            var buffer = bitmap as byte[];
-            if (buffer == null)
-                throw new ArgumentException("無効なビットマップオブジェクトです", nameof(bitmap));
-
-            File.WriteAllBytes(filePath, buffer);
+            var wrapper = ValidateAndGetImageWrapper(bitmap);
+            wrapper.Image.Save(filePath, new PngEncoder());
         }
 
         /// <summary>
@@ -76,11 +109,8 @@ namespace Amatsukaze.Lib
         /// </summary>
         public void SaveBitmapAsJpegToStream(object bitmap, Stream stream)
         {
-            var buffer = bitmap as byte[];
-            if (buffer == null)
-                throw new ArgumentException("無効なビットマップオブジェクトです", nameof(bitmap));
-
-            stream.Write(buffer, 0, buffer.Length);
+            var wrapper = ValidateAndGetImageWrapper(bitmap);
+            wrapper.Image.Save(stream, new JpegEncoder { Quality = 90 });
         }
 
         /// <summary>
@@ -88,11 +118,20 @@ namespace Amatsukaze.Lib
         /// </summary>
         public void SaveBitmapAsPngToStream(object bitmap, Stream stream)
         {
-            var buffer = bitmap as byte[];
-            if (buffer == null)
-                throw new ArgumentException("無効なビットマップオブジェクトです", nameof(bitmap));
+            var wrapper = ValidateAndGetImageWrapper(bitmap);
+            wrapper.Image.Save(stream, new PngEncoder());
+        }
 
-            stream.Write(buffer, 0, buffer.Length);
+        /// <summary>
+        /// 渡されたオブジェクトが正しいImageWrapperかチェックして取得します
+        /// </summary>
+        private ImageWrapper ValidateAndGetImageWrapper(object bitmap)
+        {
+            var wrapper = bitmap as ImageWrapper;
+            if (wrapper == null)
+                throw new ArgumentException("無効なビットマップオブジェクトです", nameof(bitmap));
+            
+            return wrapper;
         }
     }
 } 
